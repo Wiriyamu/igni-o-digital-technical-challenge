1462744b5b184ff5f0f18911ac21ecc68356a0f4
Willian Silva <will.devfrontend@gmail.com>
2021-03-22 13:38:09 -0300

    :sparkles: feat: add TheSidebar and TheDashboard components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
-  <div></div>
+  <main class="app">
+    <the-sidebar />
+    <the-dashboard />
+  </main>
 </template>
 
 <script>
+import TheDashboard from './components/TheDashboard.vue'
+import TheSidebar from './components/TheSidebar.vue'
+
 export default {
+  components: { TheSidebar, TheDashboard },
   name: 'App'
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  height: 100vh;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+}
+
+.app {
+  display: flex;
+  background-color: orange;
+}
+</style>
